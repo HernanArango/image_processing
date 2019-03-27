314611de2653ec18df4e7b9d93cb399d1d5d94d9
Hernan <hernantion1@yahoo.com>
2019-03-27 17:51:43 -0500

    escogiendo color

--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,9 @@
 from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import ttk
 from tkinter import *
+
+
 import pydicom
 #import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -23,24 +27,36 @@ class Interfaz:
 		else:
 			self.image = cv2.imread(self.filename)
 			
-	def execute(self,function,color = 1):
-		
+	def execute(self,function,color = 1,*options):
+
+		#progress= ttk.Progressbar(self.root, orient = 'horizontal', maximum = 100, mode = 'indeterminate')
+		#progress.start()
+		#progress.pack(fill=BOTH)
 		if self.typefile == "dcm":
-			return function(self.image)
+			if options:
+				return function(self.image,options)
+			else:
+				return function(self.image)
 		else:
 			if color == 0:
 				#grayscale
 				new_matriz = []
 				img = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 				#cv2.imwrite("test.png", img)
-				new_image = function(img)
+				if options:
+					new_image = function(img,options)
+				else:
+					new_image = function(img)
 				return new_image
 			else:
 				new_matriz = []
 				b,g,r = cv2.split (self.image)
 				chanels_image = [b,g,r]
 				for chanel in chanels_image:
-					new_matriz.append(function(chanel))
+					if options:
+						new_matriz.append(function(chanel,options))
+					else:
+						new_matriz.append(function(chanel))
 
 				new_image = cv2.merge([new_matriz[0],new_matriz[1],new_matriz[2]])
 				cv2.imwrite("test.png", new_image)
@@ -55,13 +71,21 @@ class Interfaz:
 
 	def gauss(self):
 		print ("ALGORITMO GAUSS")
-		new_image = self.execute(gauss,0)
+		neighbours = simpledialog.askstring("", "Número de vecinos",
+		parent=self.root)
+		sigma = simpledialog.askstring("", "Sigma",
+		parent=self.root)
+		new_image = self.execute(gauss,0,neighbours,sigma)
 		#new_image = cv2.GaussianBlur(self.image,(5,5),0)
 		self.show(self.image,new_image)
 
 	def raleight(self):
 		print ("ALGORITMO RALEIGHT")
-		new_image = self.execute(raleight,0)
+		neighbours = simpledialog.askstring("", "Número de vecinos",
+		parent=self.root)
+		sigma = simpledialog.askstring("", "Sigma",
+		parent=self.root)
+		new_image = self.execute(raleight,0,neighbours,sigma)
 		self.show(self.image,new_image)
 
 	def mediana(self):
@@ -81,7 +105,9 @@ class Interfaz:
 
 	def kmeans(self):
 		print("KMEANS")
-		new_image = self.execute(kmeans,0)
+		centroides = simpledialog.askstring("", "Número de centroides",
+		parent=self.root)
+		new_image = self.execute(kmeans,0,centroides)
 		self.show(self.image,new_image)
 
 	def otsu(self):
@@ -102,19 +128,19 @@ class Interfaz:
 		plt.show()
 
 	def show_img(self):
-		dataset = pydicom.dcmread(self.filename)
-		img =dataset.pixel_array
+		if self.canvas != 0:
+			#self.canvas.delete('all')
+			self.canvas.get_tk_widget().destroy()
 		fig = Figure(figsize=(6,6))
 		plot=fig.add_subplot(111)
-		plot.imshow(img,cmap=plt.cm.bone),plt.title('Original')
+		plot.imshow(self.image,cmap=plt.cm.bone),plt.title('Original')
 		self.canvas = FigureCanvasTkAgg(fig, master=self.root)
 		self.canvas.get_tk_widget().pack()
 		self.canvas.draw()
-		"""
-		plt.imshow(img,cmap=plt.cm.bone),plt.title('Original')
-		plt.show()
-		"""
+
 	def show(self,img,new_img):
+		if self.canvas != 0:
+			self.canvas.get_tk_widget().destroy()
 		fig = Figure(figsize=(6,6))
 		plt1=fig.add_subplot(121)
 		plt2=fig.add_subplot(122)
@@ -134,11 +160,24 @@ class Interfaz:
 		plt.subplot(122),plt.imshow(new_img,cmap=plt.cm.bone),plt.title('Convolucion')
 		plt.xticks([]), plt.yticks([])
 		plt.show()
-		"""
 	
+		"""
+	def change_color(self,option):
+		print("color y gray",self.color.get(),self.grayscale.get())
+
+		if option == 0:
+			self.color.set(False)
+			self.grayscale.set(True)
+		else:
+			self.color.set(True)
+			self.grayscale.set(False)
+
+
+
 	def __init__(self):
 		self.root = Tk()
 		self.root.geometry("500x500")
+		self.root.title("Proyecto Imagenes")
 		 
 		menubar = Menu(self.root)
 		self.root.config(menu=menubar)
@@ -149,33 +188,51 @@ class Interfaz:
 		filemenu.add_command(label="Salir", command=self.root.quit)
 
 
-		show_all = BooleanVar()
-		show_all.set(True)
-		show_done = BooleanVar()
-		show_not_done = BooleanVar()
-
-		view_menu = Menu(menubar)
-		view_menu.add_checkbutton(label="eliminar bordes", onvalue=1, offvalue=False, variable=show_all)
-		view_menu.add_checkbutton(label="otro", onvalue=True, offvalue=0, variable=show_done)
-		#view_menu.add_checkbutton(label="Show Not Done", onvalue=1, offvalue=0, variable=show_not_done)
+		self.color = BooleanVar()
+		self.color.set(True)
+		self.grayscale= BooleanVar()
+		self.grayscale.set(False)
 
 
-
-		editmenu = Menu(menubar, tearoff=0)
-		editmenu.add_command(label="Ver imagen",command=self.show_img)
-		editmenu.add_command(label="Gauss",command=self.gauss)
-		editmenu.add_command(label="Raleight",command=self.raleight)
-		editmenu.add_command(label="Mediana", command=self.mediana)
-		editmenu.add_command(label="Sobel", command=self.sobel)
-		editmenu.add_command(label="Dilatacion", command=self.expansion)
-		editmenu.add_command(label="Erosion", command=self.erosion)
-		editmenu.add_command(label="Kmeans", command=self.kmeans)
-		editmenu.add_command(label="Histograma", command=self.histogram)
-		editmenu.add_command(label="Otsu", command=self.otsu)
+		
 		
 
-		 
-		 
+		view_menu = Menu(menubar)
+		view_menu.add_command(label="Ver imagen",command=self.show_img)
+		view_menu.add_command(label="Histograma", command=self.histogram)
+		view_menu.add_separator()
+		view_menu.add_checkbutton(label="Escala de grises", onvalue=1, offvalue=False, command= lambda :self.change_color(0),variable = self.grayscale)
+		view_menu.add_checkbutton(label="Color", onvalue=True, offvalue=0, command=lambda: self.change_color(1),variable = self.color)
+		
+
+		editmenu = Menu(menubar, tearoff=0)
+			
+		
+		
+
+		morfologicas = Menu(editmenu, tearoff=0)
+		morfologicas.add_command(label="Dilatacion", command=self.expansion)
+		morfologicas.add_command(label="Erosion", command=self.erosion)
+		editmenu.add_cascade(label="Operaciones Morfologicas", menu=morfologicas)
+
+		filtros = Menu(editmenu, tearoff=0)
+		filtros.add_command(label="Raleight",command=self.raleight)
+		filtros.add_command(label="Mediana", command=self.mediana)
+		filtros.add_command(label="Gauss",command=self.gauss)
+		editmenu.add_cascade(label="Filtros", menu=filtros)
+
+		umbral = Menu(editmenu, tearoff=0)
+		umbral.add_command(label="Otsu", command=self.otsu)
+		editmenu.add_cascade(label="Umbral", menu=umbral)
+
+		agrupacion = Menu(editmenu, tearoff=0)
+		agrupacion.add_command(label="Kmeans", command=self.kmeans)
+		editmenu.add_cascade(label="Agrupación", menu=agrupacion)
+		
+		bordes = Menu(editmenu, tearoff=0)
+		bordes.add_command(label="Sobel", command=self.sobel)
+		editmenu.add_cascade(label="Bordes", menu=bordes)
+
 		helpmenu = Menu(menubar, tearoff=0)
 		helpmenu.add_command(label="Ayuda")
 		helpmenu.add_separator()
@@ -186,7 +243,7 @@ class Interfaz:
 		menubar.add_cascade(label='Opciones', menu=view_menu)
 		menubar.add_cascade(label="Ayuda", menu=helpmenu)
 
-
+		self.canvas = 0
 		self.root.mainloop()
 
 
--- tools/kernel.py
+++ tools/kernel.py
@@ -102,117 +102,123 @@ def sobel(img):
 	return new_img
 
 
-def gauss(img):
+def gauss(img,options):
 	#matriz_convolucion = np.matrix("0, 1, 0; 1, -4, 1; 0, 1, 0")
 
-	matriz_convolution = get_gaussian_filter()
+	matriz_convolution = get_gaussian_filter(options)
 
 	new_img = convolution(img,matriz_convolution)
 	return new_img
 
 
-def raleight(img):
-	matriz_convolution = get_rayleigh_filter()
+def raleight(img,options):
+	matriz_convolution = get_rayleigh_filter(options)
 	return convolution(img,matriz_convolution)
 
 
-def get_gaussian_filter(neighbours = 1, sigma = 1):
-    '''
-    This function calculates a NxN gaussian filter; N = neighbours * 2 + 1
-    ----------
-    G(X,Y) = (1/ 2 * PI * SIGMA) * e^(-(X^2 + Y^2) / 2 * SIGMA^2)
-    ----------
-    The values of X and Y are the difference between the current pixel and the center of the kernel
-    For a 3x3 kernel, we have the following values of X and Y paired as tuples.
-    ----------
-    [
-        [(-1,-1), (0, -1) (1, -1)],
-        [(-1, 0), (0, 0), (1, 0)],
-        [(-1, 1), (0, 1), (1, 1)]
-    ]
-    '''
-    N =  neighbours * 2 + 1
+def get_gaussian_filter(options):
+	neighbours = int(options[0])
+	sigma = int(options[1])
+	'''
+	This function calculates a NxN gaussian filter; N = neighbours * 2 + 1
+	----------
+	G(X,Y) = (1/ 2 * PI * SIGMA) * e^(-(X^2 + Y^2) / 2 * SIGMA^2)
+	----------
+	The values of X and Y are the difference between the current pixel and the center of the kernel
+	For a 3x3 kernel, we have the following values of X and Y paired as tuples.
+	----------
+	[
+	    [(-1,-1), (0, -1) (1, -1)],
+	    [(-1, 0), (0, 0), (1, 0)],
+	    [(-1, 1), (0, 1), (1, 1)]
+	]
+	'''
+	N =  neighbours * 2 + 1
 
-    #First, we create an array of shape NxN
-    X = Y = np.zeros((N, N))
+	#First, we create an array of shape NxN
+	X = Y = np.zeros((N, N))
 
-    #Then, we populate all the columns using np.arange - neighbours, to get the distance
-    #between the element in the middle (index = neigboors) and the index given by arange
+	#Then, we populate all the columns using np.arange - neighbours, to get the distance
+	#between the element in the middle (index = neigboors) and the index given by arange
 
-    #NOTE: If you want to change the indexes, similar to the Rayleigh function, change to the following line:
-    #Y[:] = np.arange(N, dtype=np.float32)
-    Y[:] = np.arange(N, dtype=np.float32) - neighbours
-    
-    #To calculate the rows, we transpose the columns
-    X = np.transpose(Y)
+	#NOTE: If you want to change the indexes, similar to the Rayleigh function, change to the following line:
+	#Y[:] = np.arange(N, dtype=np.float32)
+	Y[:] = np.arange(N, dtype=np.float32) - neighbours
 
-    #NOTE: We do this to use numpy's vectorization, since we can have X and Y index values separated. 
-    #Remember that the Gaussian filter in 2D is equal to multiplying two 1D Gaussians
+	#To calculate the rows, we transpose the columns
+	X = np.transpose(Y)
 
-    gaussian_filter = np.zeros((N, N))
+	#NOTE: We do this to use numpy's vectorization, since we can have X and Y index values separated. 
+	#Remember that the Gaussian filter in 2D is equal to multiplying two 1D Gaussians
 
-    #We write down the left side (scalar value) of the equation: (1/ 2 * PI * SIGMA)
-    left_side = (1 / (np.pi * 2 * np.power(sigma, 2))) 
+	gaussian_filter = np.zeros((N, N))
 
-    #We start writting down the right side: -(X^2 + Y^2) / 2 * SIGMA^2
-    aux = -(np.power(X, 2) + np.power(Y, 2)) / (2 * np.power(sigma, 2))
-    #Finally, we use the Exp function to calculate the e^(aux)
-    right_side = np.exp(aux)
+	#We write down the left side (scalar value) of the equation: (1/ 2 * PI * SIGMA)
+	left_side = (1 / (np.pi * 2 * np.power(sigma, 2))) 
 
-    #Multiply both sides of the equation
-    gaussian_filter = left_side * right_side
+	#We start writting down the right side: -(X^2 + Y^2) / 2 * SIGMA^2
+	aux = -(np.power(X, 2) + np.power(Y, 2)) / (2 * np.power(sigma, 2))
+	#Finally, we use the Exp function to calculate the e^(aux)
+	right_side = np.exp(aux)
 
-    #To calculate the scalar value to multiply by, we sum over all the axes of the gaussian matrix, using np.sum
-    factor_value = 1 / np.sum(gaussian_filter)
+	#Multiply both sides of the equation
+	gaussian_filter = left_side * right_side
 
-    return gaussian_filter#, factor_value
+	#To calculate the scalar value to multiply by, we sum over all the axes of the gaussian matrix, using np.sum
+	factor_value = 1 / np.sum(gaussian_filter)
 
-def get_rayleigh_filter(neighbours = 1, sigma = 1):
-    '''
-    This function calculates a NxN rayleigh filter; N = neighbours * 2 + 1
-    ----------
-    G(X,Y) = [X * Y / SIGMA^4] * [e^(-(X^2 + Y^2) / 2 * SIGMA^2))]
-    ----------
-    The values of X and Y are the indixes of each pixel
-    For a 3x3 kernel, we have the following values of X and Y paired as tuples.
-    ----------
-    [
-        [(0,0), (0, 1) (0, 2)],
-        [(1, 0), (1, 1), (1, 2)],
-        [(2, 0), (2, 1), (2, 2)]
-    ]
-    '''
-    N =  neighbours * 2 + 1
+	return gaussian_filter#, factor_value
 
-    #First, we create an array of shape NxN
-    X = Y = np.zeros((N, N))
+def get_rayleigh_filter(options):
+	
+	neighbours = int(options[0])
+	sigma = int(options[1])
 
-    #Then, we populate all the columns using np.arange to get the indexes
-    Y[:] = np.arange(N, dtype=np.float32)
-    
-    #To calculate the rows, we transpose the columns
-    X = np.transpose(Y)
+	'''
+	This function calculates a NxN rayleigh filter; N = neighbours * 2 + 1
+	----------
+	G(X,Y) = [X * Y / SIGMA^4] * [e^(-(X^2 + Y^2) / 2 * SIGMA^2))]
+	----------
+	The values of X and Y are the indixes of each pixel
+	For a 3x3 kernel, we have the following values of X and Y paired as tuples.
+	----------
+	[
+	    [(0,0), (0, 1) (0, 2)],
+	    [(1, 0), (1, 1), (1, 2)],
+	    [(2, 0), (2, 1), (2, 2)]
+	]
+	'''
+	N =  neighbours * 2 + 1
 
-    #NOTE: We do this to use numpy's vectorization, since we can have X and Y index values separated. 
-    #Similar to gaussian 2D Rayleighs are equal to multiplying two 1D Rayleighs
-    rayleigh_filter = np.zeros((N, N))
+	#First, we create an array of shape NxN
+	X = Y = np.zeros((N, N))
 
-    #We write down the left side of the equation: [X * Y / SIGMA^4]
-    left_side = np.divide(X * Y, np.power(sigma, 4))
+	#Then, we populate all the columns using np.arange to get the indexes
+	Y[:] = np.arange(N, dtype=np.float32)
 
-    #We start writting down the right side: -(X^2 + Y^2) / 2 * SIGMA^2
-    aux = -(np.power(X, 2) + np.power(Y, 2)) / (2 * np.power(sigma, 2))
-    #Finally, we use the Exp function to calculate the e^(aux)
-    right_side = np.exp(aux)
+	#To calculate the rows, we transpose the columns
+	X = np.transpose(Y)
+
+	#NOTE: We do this to use numpy's vectorization, since we can have X and Y index values separated. 
+	#Similar to gaussian 2D Rayleighs are equal to multiplying two 1D Rayleighs
+	rayleigh_filter = np.zeros((N, N))
+
+	#We write down the left side of the equation: [X * Y / SIGMA^4]
+	left_side = np.divide(X * Y, np.power(sigma, 4))
+
+	#We start writting down the right side: -(X^2 + Y^2) / 2 * SIGMA^2
+	aux = -(np.power(X, 2) + np.power(Y, 2)) / (2 * np.power(sigma, 2))
+	#Finally, we use the Exp function to calculate the e^(aux)
+	right_side = np.exp(aux)
 
 
-    #Multiply both sides of the equation
-    rayleigh_filter = left_side * right_side
+	#Multiply both sides of the equation
+	rayleigh_filter = left_side * right_side
 
-    #To calculate the scalar value to multiply by, we sum over all the axes of the gaussian matrix, using np.sum
-    factor_value = 1 / np.sum(rayleigh_filter)
+	#To calculate the scalar value to multiply by, we sum over all the axes of the gaussian matrix, using np.sum
+	factor_value = 1 / np.sum(rayleigh_filter)
 
-    return rayleigh_filter #, factor_value
+	return rayleigh_filter #, factor_value
 
 def mediana(img):
 
@@ -348,8 +354,8 @@ def erosion(img):
 
 	return new_matriz
 
-def kmeans(img, k = 3):
-
+def kmeans(img, k):
+	k = int(k[0])
 	rows, cols = img.shape
 	new_matriz = np.zeros((rows, cols))
 
